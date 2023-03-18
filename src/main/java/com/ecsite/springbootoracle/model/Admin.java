729f2193899668be3c3b4dc1c241784ad73e39e2
nguyenmanhcong <cng.buff@gmail.com>
2023-03-18 14:37:00 +0900

    Create entity + dto of Role.java and Admin.java

--- src/main/java/com/ecsite/springbootoracle/model/Admin.java
+++ src/main/java/com/ecsite/springbootoracle/model/Admin.java
@@ -1,4 +1,29 @@
 package com.ecsite.springbootoracle.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "admins")
 public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "admin_id")
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "admins_rolos", joinColumns = @JoinColumn(name = "admin_id", referencedColumnName = "admin_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "role_id"))
+    private Collection<Role> roles;
 }
--- src/main/java/com/ecsite/springbootoracle/model/Role.java
+++ src/main/java/com/ecsite/springbootoracle/model/Role.java
@@ -1,4 +1,26 @@
 package com.ecsite.springbootoracle.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+//    @SequenceGenerator(name = "role_seq", sequenceName = "ROLE_SEQ", allocationSize = 1)
+    @Column(name = "role_id")
+    private Long id;
+
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
 }
