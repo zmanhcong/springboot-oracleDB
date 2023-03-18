862572a40618b681d6df8c6efa68c55faf4b1c95
nguyenmanhcong <cng.buff@gmail.com>
2023-03-18 14:38:35 +0900

    Repository: create repository of ADMIN and ROLE

---
+++ src/main/java/com/ecsite/springbootoracle/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.ecsite.springbootoracle.repository;
+
+import com.ecsite.springbootoracle.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
