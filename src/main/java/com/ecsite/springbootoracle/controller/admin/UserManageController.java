2c7341cc4d0dacec7b9b11d037116337bd8c5bcf
nguyenmanhcong <cng.buff@gmail.com>
2023-03-21 22:06:52 +0900

    Admin/User-list: add user list page

---
+++ src/main/java/com/ecsite/springbootoracle/controller/admin/UserManageController.java
@@ -0,0 +1,31 @@
+package com.ecsite.springbootoracle.controller.admin;
+
+import com.ecsite.springbootoracle.model.Admin;
+import com.ecsite.springbootoracle.repository.AdminRepository;
+import com.ecsite.springbootoracle.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("admin")
+public class UserManageController {
+    @Autowired
+    AdminService adminService;
+
+    @Autowired
+    AdminRepository adminRepository;
+
+    @GetMapping("/user/list")
+    public String userList(){
+        return "/admin/userlist";
+    }
+
+}
