aa2aa0111608caf0d53e9ea13a8ee0cbb7b3bfd3
nguyenmanhcong <cng.buff@gmail.com>
2023-03-24 01:02:15 +0900

    Fixbug/Active user and send when notify acount new status of user(active or inactiv)

--- src/main/java/com/ecsite/springbootoracle/controller/admin/UserController.java
+++ src/main/java/com/ecsite/springbootoracle/controller/admin/UserController.java
@@ -1,20 +1,14 @@
 package com.ecsite.springbootoracle.controller.admin;
-
-import com.ecsite.springbootoracle.dto.AdminDto;
 import com.ecsite.springbootoracle.model.Admin;
 import com.ecsite.springbootoracle.repository.AdminRepository;
 import com.ecsite.springbootoracle.service.AdminService;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.transaction.Transactional;
 import java.util.Optional;
-
 
 @Controller
 //@RequestMapping("admin")
@@ -29,31 +23,23 @@ public class UserController {
     @Autowired
     private JavaMailSender mailSender;
 
-
     @GetMapping("activateUser/{id}")
     public String activateUser(@PathVariable("id") Long id, Model model) {
-        Optional<Admin> user = adminRepository.findById(id);
-        AdminDto adminDto = new AdminDto();
-        if (user.isPresent()) {
-            Admin admin = user.get();
-            BeanUtils.copyProperties(admin, adminDto);
-            adminDto.setActive(!adminDto.isActive());
-            adminDto.setAccountRole("user");
-            Admin updatedAdmin = userService.update(adminDto);
-            if (updatedAdmin == null) {
-                model.addAttribute("errorMessage", "User not found");
-                return "redirect:/admin/userlist";
-            }
-            sendActivationEmail(adminDto);
+        Optional<Admin> optionalAdmin = adminRepository.findById(id);
+        if (optionalAdmin.isPresent()) {
+            Admin admin = optionalAdmin.get();
+            admin.setActive(!admin.isActive()); //change status of user.
+            adminRepository.save(admin);
+            sendActivationEmail(admin);
 
             model.addAttribute("successMessage", "User status updated");
-            return "redirect:/admin/userlist";
+            return "redirect:/admin/user/list";
         }
         model.addAttribute("errorMessage", "User not found");
-        return "redirect:/admin/userlist";
+        return "redirect:/admin/user/list";
     }
 
-    private void sendActivationEmail(AdminDto user) {
+    private void sendActivationEmail(Admin user) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(user.getUsername());
         message.setSubject("Account Status Changed");
@@ -63,7 +49,6 @@ public class UserController {
         } else {
             message.setText("Hello " + user.getFirstName() + ",\n\nYour account has been deactivated.\n\nThank you!");
         }
-
         mailSender.send(message);
     }
 }
